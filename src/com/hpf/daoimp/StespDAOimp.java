191089d5345613429dde75e5f46c4e72bda088df
汪哲逸 <wangzheyi@hme.cn>
2017-06-15 11:33:05 +0800

    完成了sql配置
    完成了sql配置

--- src/com/hpf/daoimp/StespDAOimp.java
+++ src/com/hpf/daoimp/StespDAOimp.java
@@ -40,7 +40,7 @@ public class StespDAOimp implements StespsDAO {
 	@Override
 	public void CompleteApplication(UserModel userModel, PayNotifyModel payNotifyModel) {
 		
-		System.out.println("userModel.getLightpay()="+userModel.getLightpay());
+/*		System.out.println("userModel.getLightpay()="+userModel.getLightpay());
 		System.out.println("userModel.getCasherSystem()="+userModel.getCasherSystem());
 		System.out.println("userModel.getCooperatetype()="+userModel.getCooperatetype());
 		System.out.println("userModel.getRestaurantName()="+userModel.getRestaurantName());
@@ -60,23 +60,43 @@ public class StespDAOimp implements StespsDAO {
 		System.out.println("userModel.getViewspath()="+userModel.getViewspath());	
 		System.out.println("userModel.getLicensepath()="+userModel.getLicensepath());
 		System.out.println("userModel.getContractpath()="+userModel.getContractpath());
-		System.out.println("userModel.getContractpath()="+userModel.getContractpath());
+		System.out.println("userModel.getAttorneypath()="+userModel.getAttorneypath());
 		System.out.println("userModel.getBankaccountName()="+userModel.getBankaccountName());
 		System.out.println("userModel.getBankaccountBank()="+userModel.getBankaccountBank());
 		System.out.println("userModel.getBankaccountAccount()="+userModel.getBankaccountAccount());
 		System.out.println("payNotifyModel.getPay_result()="+payNotifyModel.getPay_result());
 		System.out.println("payNotifyModel.getTotal_fee()="+payNotifyModel.getTotal_fee());
 		System.out.println("payNotifyModel.getTime_end()="+payNotifyModel.getTime_end());
-		System.out.println("payNotifyModel.getOut_trade_no()="+payNotifyModel.getOut_trade_no());
+		System.out.println("payNotifyModel.getOut_trade_no()="+payNotifyModel.getOut_trade_no());*/
 		
 		
-/*		String sql = "INSERT INTO application"
-				+ "(name, email, content,sentdate , status) VALUES(?,?,?,?,?)";
+		String sql = "INSERT INTO ec_online_sign"
+				+ "(lightPay, casherSystem, cooperateType,restaurantName , restaurantType"
+				+ ",restaurantProvince,restaurantCity,restaurantDistrict,restaurantStreet"
+				+ ",restaurantAddress,restaurantTel"
+				+ ",restaurantOpenTime,restaurantCloseTime,restaurantIndroduction"
+				+ ",managerPhone,bossPhone,restaurantViewsURL,baseURL,licenseURL"
+				+ ",contractURL,attorneyURL,bankAccountName,bankAccountBank,bankAccountAccount"
+				+ ",total_fee,pay_result,out_trade_no,time_end"
+				+ ") VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 		
 		JdbcTemplate jdbcTemplate = new JdbcTemplate(datasource);
 		
-		jdbcTemplate.update( sql,);
-		*/
+	jdbcTemplate.update( sql,userModel.getLightpay(),userModel.getCasherSystem(),
+			userModel.getRestaurantName(),userModel.getCooperatetype(),
+			userModel.getRestaurantProvince(),userModel.getRestaurantCity(),
+			userModel.getRestaurantDistrict(),userModel.getRestaurantStreet(),
+			userModel.getRestaurantAddress(),userModel.getRestaurantTel(),
+			userModel.getRestaurantOpentime(),userModel.getRestaurantClosetime(),
+			userModel.getRestaurantIndroduction(),userModel.getManagerPhone(),
+			userModel.getBossPhone(),userModel.getBaseurl(),userModel.getViewspath(),
+			userModel.getLicensepath(),userModel.getContractpath(),userModel.getAttorneypath(),			
+			userModel.getBankaccountName(),userModel.getBankaccountBank(),
+			userModel.getBankaccountAccount(),payNotifyModel.getTotal_fee(),
+			payNotifyModel.getPay_result(), payNotifyModel.getOut_trade_no(),
+			payNotifyModel.getTime_end()
+			);
+	
 		
 	}
 
